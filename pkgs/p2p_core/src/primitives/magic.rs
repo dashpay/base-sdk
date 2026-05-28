7192687555bb19029893a2ae879944851ea2d842
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-05-29 00:47:19 +0530

    sdk%refac(codec): remove avoidable scaffolding for `Magic`

--- pkgs/p2p_core/src/primitives/magic.rs
+++ pkgs/p2p_core/src/primitives/magic.rs
@@ -6,56 +6,9 @@
 
 //! Dash network magic bytes.
 
-use dash_types::impl_bytes;
+use dash_types::make_bytes;
 
-use core::fmt;
-
-/// Four-byte network identifier prepended to every V1 message.
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
-#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
-pub struct Magic(pub [u8; 4]);
-
-impl Magic {
-  /// Returns the inner byte array.
-  pub const fn to_bytes(self) -> [u8; 4] {
-    self.0
-  }
-
-  /// Borrows the inner byte array.
-  pub const fn as_bytes(&self) -> &[u8; 4] {
-    &self.0
-  }
+make_bytes! {
+  /// Four-byte network identifier prepended to every V1 message.
+  Magic, 4
 }
-
-impl From<Magic> for [u8; 4] {
-  fn from(val: Magic) -> Self {
-    val.0
-  }
-}
-
-impl AsRef<[u8]> for Magic {
-  fn as_ref(&self) -> &[u8] {
-    &self.0
-  }
-}
-
-impl fmt::Debug for Magic {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "Magic(")?;
-    for byte in &self.0 {
-      write!(f, "{:02x}", byte)?;
-    }
-    write!(f, ")")
-  }
-}
-
-impl fmt::Display for Magic {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    for byte in &self.0 {
-      write!(f, "{:02x}", byte)?;
-    }
-    Ok(())
-  }
-}
-
-impl_bytes!(4, Magic);
