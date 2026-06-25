405df6040b57b4a5227aad8cfea646d6572a07d2
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-06-25 14:35:07 +0530

    p2p_core%refac: drop `Hash` derive from `DashNetworkMessage`

--- pkgs/p2p_core/src/msg/mod.rs
+++ pkgs/p2p_core/src/msg/mod.rs
@@ -81,7 +81,7 @@ macro_rules! define_network_messages {
     /// not-yet-implemented messages use `Vec<u8>` to hold the raw
     /// payload so callers can identify *what* was received (for
     /// logging) without needing a full decoder.
-    #[derive(Clone, Debug, Eq, Hash, PartialEq, Unencodable)]
+    #[derive(Clone, Debug, Eq, PartialEq, Unencodable)]
     #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
     pub enum DashNetworkMessage {
       $( $(#[$p_doc])* $p_variant($p_type), )*
