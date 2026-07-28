22890b0948173a6210c8fb8c674e21a75c58d35d
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-07-28 05:49:56 +0530

    pow%refac(test): migrate to `Corpus`-formatted test vectors

--- pkgs/pow/tests/echo.rs
+++ pkgs/pow/tests/echo.rs
@@ -6,8 +6,6 @@
 
 //! Echo-512 tests.
 
-#![expect(clippy::panic, reason = "test code")]
-
 mod common;
 
 #[cfg(feature = "simd")]
--- pkgs/pow/tests/groestl.rs
+++ pkgs/pow/tests/groestl.rs
@@ -6,7 +6,6 @@
 
 //! Groestl-512 tests.
 
-#![expect(clippy::panic, reason = "test code")]
 #![cfg_attr(feature = "simd", feature(portable_simd))]
 
 mod common;
